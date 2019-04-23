5d87bb16d2099abc3c99fce7dfa3370bff960d48
parvika <psingha4@asu.edu>
2019-04-22 20:49:04 -0700

    fin.

--- js/autoComplete.js
+++ js/autoComplete.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    showChart("Symptom");
-    show_time_series("Symptom");
+    //showChart("Symptom");
+    //show_time_series("Symptom");
     var topics = [
         "Suicidal",
         "Asymptomatic",
