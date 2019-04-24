b005dad78fd8c67cb851573cd2a45a800762b520
Mayur Padaval <mayur.padaval@gmail.com>
2019-04-24 13:32:37 -0700

    fixed the wiki link issue

--- js/Details.js
+++ js/Details.js
@@ -1,12 +1,12 @@
 function add_details(keyword) {
     d3.json("processedData/data3.json").then(function (data) {
-        console.log(data[keyword]);
+        //console.log(data[keyword]);
         document.getElementById("details").textContent  = data[keyword].content
         var element = document.createElement("a");
         element.setAttribute("href", data[keyword].url);
-        element.innerHTML = "More Info"
-
-        document.getElementById("details2").replaceWith(element);
+        element.innerHTML = "More Info";
+        //document.getElementById("details2").replaceWith(element);
+        document.getElementById("more_info").href = data[keyword].url;
     });
     
 }
